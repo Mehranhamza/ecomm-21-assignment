c2168314cdef6dcce705df78cb352d3eb6ffc58b
Mehran Hamza <Mehranhamza8@gmail.com>
2024-10-19 00:18:12 +0500

    update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -37,7 +37,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "2b0f3f29c4d4f06b98eb6d3a7649da7b
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "b8517d54112ae7c6afb64d33848b3f02",
+"flutter_bootstrap.js": "42894dfb6f2eb0e9556d35307759b098",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
